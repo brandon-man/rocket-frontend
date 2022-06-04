9a24418c8aa26a505e956c017ca4e2bd29fc594b
brandon-man <nybrandonman@gmail.com>
2022-06-04 00:02:17 -0700

    Wallet

--- components/Home.js
+++ components/Home.js
@@ -1,4 +1,5 @@
-import { Box, Button, Container, Heading, Text } from "@chakra-ui/react";
+import { Button, Container, Flex, Heading, Text } from "@chakra-ui/react";
+import { useEffect } from "react";
 import { ethers } from "ethers";
 
 function Home() {
@@ -6,13 +7,13 @@ function Home() {
 
   return (
     <Container>
-      <Box>
-        <Heading>🚀 Hey there!</Heading>
+      <Flex alignItems="center" flexDirection="column" m={4}>
+        <Heading p={4}>🚀 Hey there!</Heading>
 
-        <Text>Connect your Ethereum wallet and go to the moon!</Text>
+        <Text p={4}>Connect your Ethereum wallet and go to the moon!</Text>
 
         <Button onClick={rocket}>To the moon!</Button>
-      </Box>
+      </Flex>
     </Container>
   );
 }
